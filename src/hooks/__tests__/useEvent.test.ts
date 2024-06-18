42f436196bc8a1e879a5e200bb3d490e7b2f100e
Juan Manuel Mogliati <mogliatijm@gmail.com>
2024-06-18 00:04:23 -0300

    test: :white_check_mark: add test for useEvent hook

---
+++ src/hooks/__tests__/useEvent.test.ts
@@ -0,0 +1,27 @@
+import { act, renderHook } from '@testing-library/react-hooks';
+import { describe, expect, it, vi } from 'vitest';
+
+import { useEvent } from '../useEvent';
+
+describe('useEvent', () => {
+  it('should execute the callback', () => {
+    const fn = vi.fn();
+    const { result } = renderHook(() => useEvent(fn));
+
+    act(() => {
+      result.current();
+    });
+
+    expect(fn).toHaveBeenCalledTimes(1);
+  });
+
+  it('should memoize the handler', () => {
+    const fn = vi.fn();
+    const { result, rerender } = renderHook(() => useEvent(fn));
+
+    const eventTriggered1 = result.current;
+    rerender();
+    const eventTriggered2 = result.current;
+    expect(eventTriggered1).toBe(eventTriggered2);
+  });
+});
